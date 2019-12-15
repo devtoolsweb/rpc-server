353542a793cae65532b99bc61e31997855a8ccf6
elhombre <elhombre.dev@gmail.com>
2019-12-16 02:43:46 +0500

    Sync with rpc-common

--- examples/test_server/test_middleware.ts
+++ examples/test_server/test_middleware.ts
@@ -1,8 +1,22 @@
 import { IRpcRequest } from '@aperos/rpc-common'
 import { RpcMiddleware } from '../../lib'
 
+interface ITestMethodParams {
+  hello: string
+}
+
+interface ICalcSumParams {
+  a: number,
+  b: number
+}
+
 export class TestMiddleware extends RpcMiddleware {
-  async testMethod (request: IRpcRequest): Promise<string> {
+  async testMethod (request: IRpcRequest<ITestMethodParams>): Promise<string> {
     return `This is a test string: ${request.params?.hello}:${Math.random()}`
+  }
+
+  async calcSum (request: IRpcRequest<ICalcSumParams>): Promise<number> {
+    const p =  request.params!
+    return p.a + p.b
   }
 }
