ce6396ffb4a1acc8c2c66bb72cd54c7517cbd065
elhombre <elhombre.dev@gmail.com>
2020-05-12 13:01:11 +0500

    Fix rollup module format, downgrade rollup version to 2.8.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,20 @@
+import module from 'module'
 import path from 'path'
 import pluginDts from 'rollup-plugin-dts'
 import pluginTypescript from 'rollup-plugin-typescript2'
 import { terser as pluginTerser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
-const external = [
-  ...Object.keys(process.binding('natives')),
-  ...Object.keys(pkg.dependencies || {})
-]
+const external = [...module.builtinModules, ...Object.keys(pkg.dependencies || {})]
 
 const targetDir = 'dist'
 
-const plugins = [pluginTypescript({
-  useTsconfigDeclarationDir: true,
-  verbosity: 1 })]
+const plugins = [
+  pluginTypescript({
+    useTsconfigDeclarationDir: true,
+    verbosity: 1
+  })
+]
 
 if (process.env.BUILD === 'production') {
   plugins.push(
@@ -26,20 +27,23 @@ if (process.env.BUILD === 'production') {
 
 export default [
   {
-    input: 'lib/index.ts',
     external,
+    input: 'lib/index.ts',
     output: {
       file: path.join(targetDir, 'index.js'),
-      format: 'cjs',
+      format: 'es',
       name: 'index',
       sourcemap: true
     },
     plugins
   },
   {
-    input: './build/lib/index.d.ts',
     external,
-    output: [{ file: path.join(targetDir, 'index.d.ts'), format: 'es' }],
+    input: './build/lib/index.d.ts',
+    output: {
+      file: path.join(targetDir, 'index.d.ts'),
+      format: 'es'
+    },
     plugins: [pluginDts({ verbose: true })]
   }
 ]
