347537fe50ef44c64a18b564e573b6857898cf84
elhombre <elhombre.dev@gmail.com>
2022-04-09 23:19:47 +0500

    Fix minor issues

--- examples/test_server/index.ts
+++ examples/test_server/index.ts
@@ -1,25 +1,27 @@
 import { IRpcBackendArgs, IRpcServer, RpcHttpServer, RpcWsServer } from '../../lib'
 import { TestBackend } from './test_backend'
 
-const host = '0.0.0.0'
+const host = 'localhost'
 const wsPort = 3001
 const httpPort = 3002
 
 const setupEventHandlers = (server: IRpcServer) => {
-    server.on('error', event =>
-        console.log('Server error:', event.errorDescription))
+    server.on('error', event => {
+        console.log('Server error:', event.errorDescription)
+    })
     server
-        .on('request', event =>
+        .on('request', event => {
             console.log(
                 `Client request from ${event.httpRequest.connection.remoteAddress}:`,
                 JSON.stringify(event.request, null, '  ')
-            ))
-
-        .on('response', event =>
+            )
+        })
+        .on('response', event => {
             console.log(
                 `Server sent response at ${new Date()} :`,
                 JSON.stringify(event.response, null, '  ')
-            ))
+            )
+        })
 }
 
 console.log(`Running http RPC server at http://${host}:${httpPort}`)
@@ -29,7 +31,7 @@ const httpServer = new RpcHttpServer({
 })
 setupEventHandlers(httpServer)
 
-console.log(`Running web socket RPC server at http://${host}:${wsPort}`)
+console.log(`Running web socket RPC server at ws://${host}:${wsPort}`)
 const wsServer = new RpcWsServer({
     host,
     port: wsPort
